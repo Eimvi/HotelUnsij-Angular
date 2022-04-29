ccfc7dd9917ae0eae2ca9370b88a1bb2873e42c0
HRadry <hernandezramirezadriana1999@gmail.com>
2022-04-29 16:23:30 -0500

    Agregado ruta de componente get-link

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { GetLinkComponent } from './components/get-link/get-link.component';
 
 const routes: Routes = [
   {
     path: '', component: AuthComponent
+  },
+  {
+    path: 'get-link', component: GetLinkComponent
   }
 ];
 
