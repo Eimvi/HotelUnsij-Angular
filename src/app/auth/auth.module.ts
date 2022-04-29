ebb8ff5e3b9472a91a8be87d8b2b0806854b60a5
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-04-29 15:53:48 -0500

    Agregado componente link-sending

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LinkSendingSuccessfulComponent } from './components/link-sending-successful/link-sending-successful.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    LinkSendingSuccessfulComponent,
+    ChangePasswordComponent,
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class AuthModule { }
