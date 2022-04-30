daae0add5f1a0dee62f635cd2a0d14e80bb9a96e
HRadry <hernandezramirezadriana1999@gmail.com>
2022-04-30 18:51:40 -0500

    Borrado componenete agregados de prueba

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { GetLinkComponent } from './components/get-link/get-link.component';
-import { LinkSendingSuccessfulComponent } from './components/link-sending-successful/link-sending-successful.component';
 
 const routes: Routes = [
   {
-    path: 'link-successful/:email', component: LinkSendingSuccessfulComponent
+    path: '', component: AuthComponent
   },
   {
     path: 'get-link', component: GetLinkComponent
-  },
-  {
-    path: '', component: AuthComponent
   }
+
 ];
 
 @NgModule({
