51bd5c69d24b1de37ac6ccbc65fdb7348238e19a
emmaGT <gtapiaemmanuel@gmail.com>
2022-04-30 20:50:10 -0500

    Agregado servicio de API

--- src/app/auth/services/profile.service.ts
+++ src/app/auth/services/profile.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap} from 'rxjs/operators';
+import { User } from '../interfaces/user';
+import { environment } from '../../../environments/environment';
+import { LoginResponse } from '../interfaces/login.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private readonly URL: string = environment.URL;
+  private profile! : LoginResponse;
+
+  constructor(private http: HttpClient) { }
+
+  login(user: User){
+    return this.http.post<LoginResponse>(`${this.URL}auth/login`,user).pipe(
+    tap(
+      (resp)=>{
+        localStorage.setItem('token',resp.accessToken);
+        this.profile = resp;
+      }
+    )
+    );
+  }
+
 }
