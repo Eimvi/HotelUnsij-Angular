629b9be113523086bdbccbf3940aa501d2147dce
HRadry <hernandezramirezadriana1999@gmail.com>
2022-05-02 11:58:46 -0500

    Agregada ruta de componente 'successful-change'

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { SuccessfulChangeComponent } from './components/successful-change/successful-change.component';
 
 const routes: Routes = [
   {
     path: '', component: AuthComponent
+  },
+  {
+    path: 'successful-change', component: SuccessfulChangeComponent
   }
 ];
 
