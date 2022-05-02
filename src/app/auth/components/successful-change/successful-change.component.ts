9cc38427faaa39746e62075761511d1bb6004bfa
HRadry <hernandezramirezadriana1999@gmail.com>
2022-05-02 14:20:42 -0500

    Borrado función ngOnInit e "implements OnInit"

--- src/app/auth/components/successful-change/successful-change.component.ts
+++ src/app/auth/components/successful-change/successful-change.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-successful-change',
   templateUrl: './successful-change.component.html',
   styleUrls: ['./successful-change.component.scss']
 })
-export class SuccessfulChangeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class SuccessfulChangeComponent{
 
 }
