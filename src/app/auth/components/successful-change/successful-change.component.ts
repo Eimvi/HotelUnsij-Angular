e6002fe4c8a4a81dd19240453fd7edd5b16b980e
HRadry <hernandezramirezadriana1999@gmail.com>
2022-05-02 12:00:57 -0500

    Agregado componente y ruta a login en el HTML

---
+++ src/app/auth/components/successful-change/successful-change.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-successful-change',
+  templateUrl: './successful-change.component.html',
+  styleUrls: ['./successful-change.component.scss']
+})
+export class SuccessfulChangeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
