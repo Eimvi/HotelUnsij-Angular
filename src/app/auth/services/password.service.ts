b790690ead7e02bdadb12c0ab482d9359ccf0988
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-03 16:32:46 -0500

    Corregido función changePassword

--- src/app/auth/services/password.service.ts
+++ src/app/auth/services/password.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Password } from '../interfaces/password.interface';
+import { PasswordReset } from '../interfaces/passwordReset.interface';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -12,7 +12,7 @@ export class PasswordService {
 
   constructor (private http: HttpClient) { }
   
-  changePassword(password: Password){
-    return this.http.patch(`${this.URL}auth/reset-password`, password);
+  changePassword(passwordReset: PasswordReset){
+    return this.http.patch(`${this.URL}auth/reset-password`, passwordReset);
   }
 }
