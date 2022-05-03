bc0c5d1716f7cfba2a7b13bb2e7cc2a615136ea4
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-03 12:53:18 -0500

    Agregado Auth Routing

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 import { LinkSendingSuccessfulComponent } from './components/link-sending-successful/link-sending-successful.component';
 
 const routes: Routes = [
   {
     path: 'link-successful/:email', component: LinkSendingSuccessfulComponent
+  },
+  {
+    path: 'change-password', component: ChangePasswordComponent
   },
   {
     path: '', component: AuthComponent
