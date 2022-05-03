6e279b8947112e401f1aa5d296afa4c27bdeeaa4
Cristopher <cristopher.emodesto@gmail.com>
2022-05-02 19:50:01 -0500

    Updated Component, Module Auth

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SuccessfulChangeComponent } from './components/successful-change/successful-change.component';
 import { GetLinkComponent } from './components/get-link/get-link.component';
@@ -11,7 +10,6 @@ import { LinkSendingSuccessfulComponent } from './components/link-sending-succes
 
 @NgModule({
   declarations: [
-    AuthComponent,
     LoginComponent,
     SuccessfulChangeComponent,
     GetLinkComponent,
