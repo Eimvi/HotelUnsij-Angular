e892453a71ce239eb8bac6916a62e0c159e6e51e
Cristopher <cristopher.emodesto@gmail.com>
2022-05-02 19:06:00 -0500

    Updated Auth Module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,13 +7,15 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SuccessfulChangeComponent } from './components/successful-change/successful-change.component';
 import { GetLinkComponent } from './components/get-link/get-link.component';
+import { LinkSendingSuccessfulComponent } from './components/link-sending-successful/link-sending-successful.component';
 
 @NgModule({
   declarations: [
     AuthComponent,
     LoginComponent,
     SuccessfulChangeComponent,
-    GetLinkComponent
+    GetLinkComponent,
+    LinkSendingSuccessfulComponent
   ],
   imports: [
     CommonModule,
