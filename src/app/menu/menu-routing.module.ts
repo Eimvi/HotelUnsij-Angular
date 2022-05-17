2995d46fe0b057ffaa1e9d1526fb30c4009020be
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-17 11:43:56 -0500

    Agregado rutas navbar y my-profile

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu.component';
+import { NavbarComponent } from '../shared/components/navbar/navbar.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
 
-const routes: Routes = [{ path: '', component: MenuComponent }];
+const routes: Routes = [
+  {
+    path: '', component: MenuComponent
+  },
+  {
+    path: 'navbar', component: NavbarComponent
+  },
+  {
+    path: 'my-profile', component: MyProfileComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
