892748c937cf72bada0dae2f9df85eaa65b2f20d
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-17 12:56:40 -0500

    Removido ruta navbar

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './menu.component';
-import { NavbarComponent } from '../shared/components/navbar/navbar.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
 
 const routes: Routes = [
   {
     path: '', component: MenuComponent
-  },
-  {
-    path: 'navbar', component: NavbarComponent
   },
   {
     path: 'my-profile', component: MyProfileComponent
