8d83e8d263128edba53e95539aa9b1cf0c8facf2
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-17 11:56:34 -0500

    Agregado componente navbar al modulo shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuRoutingModule } from '../menu/menu-routing.module';
 
 
 @NgModule({
   declarations: [
+    NavbarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MenuRoutingModule
+  ],
+  exports: [
+    NavbarComponent
   ]
 })
 export class SharedModule { }
