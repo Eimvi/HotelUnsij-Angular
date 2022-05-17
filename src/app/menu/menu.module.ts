d289ece8f61c802d44daf7542a92211f31d2f4d3
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-17 11:48:44 -0500

    Agregado componente my-profile y SharedModule

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { MenuRoutingModule } from './menu-routing.module';
 import { MenuComponent } from './menu.component';
+import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    MenuComponent
+    MenuComponent,
+    MyProfileComponent
   ],
   imports: [
     CommonModule,
-    MenuRoutingModule
+    MenuRoutingModule,
+    SharedModule
   ]
 })
 export class MenuModule { }
