62cf2fa2a2a468a82dc8da22918172cab2cb081b
Cristopher <cristopher.emodesto@gmail.com>
2022-05-18 13:14:05 -0500

    Updated Route

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -9,6 +9,7 @@ import { GetLinkComponent } from './components/get-link/get-link.component';
 import { LinkSendingSuccessfulComponent } from './components/link-sending-successful/link-sending-successful.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ChangePasswordComponent } from './components/change-password/change-password.component';
+import { RegisterSuccessfulComponent } from './components/register-successful/register-successful.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { ChangePasswordComponent } from './components/change-password/change-pas
     LinkSendingSuccessfulComponent,
     RegisterComponent,
     LinkSendingSuccessfulComponent,
-    ChangePasswordComponent
+    ChangePasswordComponent,
+    RegisterSuccessfulComponent
   ],
   imports: [
     CommonModule,
