ed4b9b07cdea9f582deb2103171dc2ae64fc1815
Cristopher <cristopher.emodesto@gmail.com>
2022-05-18 13:13:25 -0500

    Added Register Successful Component

---
+++ src/app/auth/components/register-successful/register-successful.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-successful',
+  templateUrl: './register-successful.component.html',
+  styleUrls: ['./register-successful.component.scss']
+})
+export class RegisterSuccessfulComponent{
+
+
+}
