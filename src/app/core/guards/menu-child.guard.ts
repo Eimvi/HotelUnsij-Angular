7aaef354898caee79a4e66e15daf161f5645ec74
Cristopher <cristopher.emodesto@gmail.com>
2022-05-18 11:17:49 -0500

    Added Guard CheckJWT

--- src/app/core/guards/menu-child.guard.ts
+++ src/app/core/guards/menu-child.guard.ts
@@ -1,31 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateChild, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import { ProfileService } from 'src/app/auth/services/profile.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuChildGuard implements CanActivateChild, CanLoad {
 
-  constructor(private router: Router) { }
-
-  canActivateChild(
-    childRoute: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): boolean {
-    const jwToken: string = localStorage.getItem('token')!;
-    if (jwToken) {
-      return true;
-    }
-    this.router.navigateByUrl('/auth/login');
-    return false;
+  constructor(private router: Router, private profileService: ProfileService) { }
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.profileService.checkJwt().pipe(
+      tap(valid => {
+        if(!valid)
+          this.router.navigateByUrl('/auth/login');
+      })
+    );
   }
   canLoad(
     route: Route,
-    segments: UrlSegment[]): boolean {
-    const jwToken: string = localStorage.getItem('token')!;
-    if (jwToken) {
-      return true;
-    }
-    this.router.navigateByUrl('/auth/login');
-    return false;
+    segments: UrlSegment[]) {
+      return this.profileService.checkJwt().pipe(
+        tap(valid => {
+          if(!valid)
+            this.router.navigateByUrl('/auth/login');
+        })
+      );
   }
 }
