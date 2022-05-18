d08b61f703b622ba223acf31abd30e09e34b1dd3
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-18 00:38:54 -0500

    Agregado ruta maid

--- src/app/menu/menu-routing.module.ts
+++ src/app/menu/menu-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChambermaidMenuComponent } from './components/chambermaid-menu/chambermaid-menu.component';
 import { MenuComponent } from './menu.component';
 
-const routes: Routes = [{ path: '', component: MenuComponent }];
+const routes: Routes = [
+  { 
+    path: '', component: MenuComponent, 
+    children: [
+      {
+        path:'maid', component: ChambermaidMenuComponent
+      },
+    ]
+  },  
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
