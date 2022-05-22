e32e4878a61a72e07acf93a68ebfb1463d073f0f
Cristopher <cristopher.emodesto@gmail.com>
2022-05-22 00:45:30 -0500

    Fixed Validators Group

--- src/app/auth/helpers/confirmed.validator.ts
+++ src/app/auth/helpers/confirmed.validator.ts
@@ -1,16 +1,8 @@
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, ValidationErrors } from "@angular/forms";
 
-export function ConfirmedValidator(controlName: string, matchingControlName: string) {
-    return (formGroup: FormGroup) => {
-        const control = formGroup.controls[controlName];
-        const matchingControl = formGroup.controls[matchingControlName];
-        if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
-            return;
-        }
-        if (control.value !== matchingControl.value) {
-            matchingControl.setErrors({ confirmedValidator: true });
-        } else {
-            matchingControl.setErrors(null); 
-        }
-    }
+export const checkPasswords = (group: AbstractControl):  ValidationErrors | null => {
+  const pass = group.get('password')?.value;
+  const confirmPass = group.get('confirmPassword')?.value;
+  return pass === confirmPass ? null : { notSame: true }
 }
+
