dac03ae626a09dbf356220830acbd265f371c6d9
Cristopher <cristopher.emodesto@gmail.com>
2022-05-22 15:20:29 -0500

    Updated Structure, Added Pagination

--- src/app/menu/components/chambermaid-menu/chambermaid-menu.component.ts
+++ src/app/menu/components/chambermaid-menu/chambermaid-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Activity } from '../../interfaces/activity.interface';
+import { ChambermaidActivitiesService } from '../../services/chambermaid-activities.service';
 
 @Component({
   selector: 'app-chambermaid-menu',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChambermaidMenuComponent implements OnInit {
 
-  constructor() { }
+  activities!: Array<Activity>;
+  pageSize: Array<number> = [];
+  actualPage: number = this.route.snapshot.queryParams['page'];
+
+  constructor(private router: Router, private route: ActivatedRoute, private chambermaidActivitiesService: ChambermaidActivitiesService) { }
 
   ngOnInit(): void {
+    this.getActivities();
+  }
+
+  pageSelected(page: number): void {
+    this.router.navigate([],
+      {
+        relativeTo: this.route,
+        queryParams: { page: page },
+        queryParamsHandling: 'merge'
+      });
+    this.actualPage = page;
+    this.getActivities();
+  }
+
+
+  getActivities(): void {
+    this.chambermaidActivitiesService.getActivities(this.actualPage).subscribe(data => {
+      this.activities = data;
+    })
   }
 
 }
