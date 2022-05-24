5e2dcae63f9e1c5fe5cede7fcb280ec883d217f0
HRadry <hernandezramirezadriana1999@gmail.com>
2022-05-24 18:22:47 -0500

    agregado componente 'previous-report'

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -7,18 +7,23 @@ import { MenuComponent } from './menu.component';
 import { ChambermaidMenuComponent } from './components/chambermaid-menu/chambermaid-menu.component';
 import { ChambermaidActivitiesComponent } from './components/chambermaid-activities/chambermaid-activities.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { PreviousReportComponent } from './components/previous-report/previous-report.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     MenuComponent,
     ChambermaidMenuComponent,
     ChambermaidActivitiesComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    PreviousReportComponent
   ],
   imports: [
     CommonModule,
     MenuRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class MenuModule { }
