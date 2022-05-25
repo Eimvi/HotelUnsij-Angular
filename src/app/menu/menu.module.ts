9d60faf8cbe61df4d00b037d0a6a9328b1e4ce11
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-25 10:55:26 -0500

    Agregado VideoCaptureComponent, VideoFileComponent

--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -7,13 +7,17 @@ import { MenuComponent } from './menu.component';
 import { ChambermaidMenuComponent } from './components/chambermaid-menu/chambermaid-menu.component';
 import { ChambermaidActivitiesComponent } from './components/chambermaid-activities/chambermaid-activities.component';
 import { MyProfileComponent } from './components/my-profile/my-profile.component';
+import { VideoCaptureComponent } from './components/video-capture/video-capture.component';
+import { VideoFileComponent } from './components/video-file/video-file.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
     ChambermaidMenuComponent,
     ChambermaidActivitiesComponent,
-    MyProfileComponent
+    MyProfileComponent,
+    VideoCaptureComponent,
+    VideoFileComponent
   ],
   imports: [
     CommonModule,
