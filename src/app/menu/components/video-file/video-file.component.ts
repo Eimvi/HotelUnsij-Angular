33b7ca2d443ea9a948a43c17d3daab7c835605eb
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-25 10:56:32 -0500

    Agregado componente VideoFile

---
+++ src/app/menu/components/video-file/video-file.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { VideoCaptureService } from '../../services/video-capture.service';
+
+@Component({
+  selector: 'app-video-file',
+  templateUrl: './video-file.component.html',
+  styleUrls: ['./video-file.component.scss']
+})
+export class VideoFileComponent implements OnInit {
+
+  MyBlob = new Blob(['test text'], {type : 'text/plain'});
+
+  constructor(private videoCaptureService: VideoCaptureService) { }
+
+  ngOnInit(): void {
+  }
+
+  getVideo(dato: any) {
+    const data: Blob = dato;
+    this.videoCaptureService.sendVideo(data);
+  }
+}
