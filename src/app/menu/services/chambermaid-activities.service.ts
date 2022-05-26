6c444f47164208360f0e49721746272a7e802439
Cristopher <cristopher.emodesto@gmail.com>
2022-05-25 21:14:13 -0500

    Added Alert Service

--- src/app/menu/services/chambermaid-activities.service.ts
+++ src/app/menu/services/chambermaid-activities.service.ts
@@ -2,9 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs/operators';
 import { Body, BodyGetActivities } from '../interfaces/activity.interface';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class ChambermaidActivitiesService {
 
   private readonly URL: string = environment.URL;
 
-  constructor(private httpClient: HttpClient, private route: ActivatedRoute) { }
+  constructor(private httpClient: HttpClient, private toastService: ToastrService) { }
 
   getActivities(page: number = 1, keyword: string = ''): Observable<Body> {
     const query = { page, keyword };
@@ -28,6 +28,8 @@ export class ChambermaidActivitiesService {
     const query = {
       id
     }
-    return this.httpClient.patch(`${this.URL}previous-report/update/`, previousReport,{params:query});
+    return this.httpClient.patch(`${this.URL}previous-report/update/`, previousReport,{params:query}).pipe(
+      tap(() => this.toastService.success('El reporte previo se guardo correctamente.'))
+    );
   }
 }
