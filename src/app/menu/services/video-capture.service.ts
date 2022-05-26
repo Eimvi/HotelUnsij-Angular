6c9c368b08c9d5e4dcdf4dc2083bfec6e4ac9b71
Cristopher <cristopher.emodesto@gmail.com>
2022-05-26 13:55:28 -0500

    Added GetImages, Transform Images

--- src/app/menu/services/video-capture.service.ts
+++ src/app/menu/services/video-capture.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { WebcamImage } from 'ngx-webcam';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,38 @@ import { Observable, Subject } from 'rxjs';
 export class VideoCaptureService {
 
   private video!: Blob;
+  private images: WebcamImage[] = [];
 
   constructor() { }
 
-  sendVideo(data: Blob){
+  sendVideo(data: Blob) {
     this.video = data;
   }
 
   getVideo() {
     return this.video;
+  }
+
+  sendImages(images: WebcamImage[]): void {
+    this.images = images;
+  }
+
+  getImages() {
+    let images: Blob[] = [];
+    for (let image of this.images) {
+      images.push(this.convertImages(image.imageAsBase64));
+    }
+    return images;
+  }
+
+  convertImages(base64Image: string) {
+
+    const byteCharacters = atob(base64Image);
+    const byteNumbers = new Array(byteCharacters.length);
+    for (let i = 0; i < byteCharacters.length; i++) {
+      byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+    const byteArray = new Uint8Array(byteNumbers);
+    return new Blob([byteArray], { type: 'image/jpeg' });
   }
 }
