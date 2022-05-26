d8ac7e5eab84997631081a32d16a74c063409a33
Cristopher <cristopher.emodesto@gmail.com>
2022-05-25 21:10:46 -0500

    Added Service Video File

--- src/app/menu/services/video-capture.service.ts
+++ src/app/menu/services/video-capture.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VideoCaptureService {
 
+  private video!: Blob;
+
   constructor() { }
 
   sendVideo(data: Blob){
-    
+    this.video = data;
+  }
+
+  getVideo() {
+    return this.video;
   }
 }
