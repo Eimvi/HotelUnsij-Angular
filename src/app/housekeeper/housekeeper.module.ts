53cb41b7151f0d9afaee4578e17aa6b98ea4f874
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-26 18:48:19 -0500

    Agregado SharedModule a housekeeper.module

--- src/app/housekeeper/housekeeper.module.ts
+++ src/app/housekeeper/housekeeper.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HousekeeperRoutingModule } from './housekeeper-routing.module';
 import { HousekeeperComponent } from './housekeeper.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HousekeeperComponent } from './housekeeper.component';
   ],
   imports: [
     CommonModule,
-    HousekeeperRoutingModule
+    HousekeeperRoutingModule,
+    SharedModule
   ]
 })
 export class HousekeeperModule { }
