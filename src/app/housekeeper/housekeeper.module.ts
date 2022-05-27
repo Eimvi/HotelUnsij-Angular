01f20b3f70100a0624674cee435ffc5edac3d7ec
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-27 11:15:48 -0500

    Agregado hosekeeper-modules a housekeeper.module

--- src/app/housekeeper/housekeeper.module.ts
+++ src/app/housekeeper/housekeeper.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
 import { HousekeeperRoutingModule } from './housekeeper-routing.module';
 import { HousekeeperComponent } from './housekeeper.component';
+import { HousekeeperModulesComponent } from './components/housekeeper-modules/housekeeper-modules.component';
 import { SharedModule } from '../shared/shared.module';
+
 
 
 @NgModule({
   declarations: [
-    HousekeeperComponent
+    HousekeeperComponent,
+    HousekeeperModulesComponent
   ],
   imports: [
     CommonModule,
