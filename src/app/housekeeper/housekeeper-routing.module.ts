8a37ae11f595dd633918cf41d433fad2b55e250b
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-27 11:16:23 -0500

    Corregido rutas housekeeper-routing

--- src/app/housekeeper/housekeeper-routing.module.ts
+++ src/app/housekeeper/housekeeper-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
 import { HousekeeperComponent } from './housekeeper.component';
+import { HousekeeperModulesComponent } from './components/housekeeper-modules/housekeeper-modules.component';
+import { MenuGuard } from '../core/guards/menu.guard';
+import { MenuChildGuard } from '../core/guards/menu-child.guard';
 
 const routes: Routes = [
   {
-    path: '', component: HousekeeperComponent
+    path: '', component: HousekeeperComponent, canActivate: [MenuGuard],
+    children: [
+      {
+          path: 'modules', component: HousekeeperModulesComponent, canActivateChild: [MenuChildGuard]
+      }
+    ]
   }
+
 ];
 
 @NgModule({
