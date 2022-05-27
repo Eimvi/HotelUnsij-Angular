5b2d3928d711daa31de20ab4698a85faa94c6ed1
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-27 11:52:20 -0500

    Corregido nombre de ruta

--- src/app/housekeeper/housekeeper-routing.module.ts
+++ src/app/housekeeper/housekeeper-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     path: '', component: HousekeeperComponent, canActivate: [MenuGuard],
     children: [
       {
-          path: 'modules', component: HousekeeperModulesComponent, canActivateChild: [MenuChildGuard]
+          path: 'housekeeper', component: HousekeeperModulesComponent, canActivateChild: [MenuChildGuard]
       }
     ]
   }
