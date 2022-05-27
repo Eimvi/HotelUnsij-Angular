53d160a7b4236d81cba57def88116c5983ab6c9e
emmaGT <gtapiaemmanuel@gmail.com>
2022-05-26 19:30:26 -0500

    Eliminados routing de Housekeeper y Menu

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MenuRoutingModule } from '../menu/menu-routing.module';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { VideoCaptureComponent } from './components/video-capture/video-capture.component';
 import { SearchComponent } from './components/search/search.component';
-import { HousekeeperRoutingModule } from '../housekeeper/housekeeper-routing.module';
 
 
 @NgModule({
@@ -18,9 +16,7 @@ import { HousekeeperRoutingModule } from '../housekeeper/housekeeper-routing.mod
   ],
   imports: [
     CommonModule,
-    FormsModule,
-    MenuRoutingModule,
-    HousekeeperRoutingModule
+    FormsModule
   ],
   exports: [
     NavbarComponent,
