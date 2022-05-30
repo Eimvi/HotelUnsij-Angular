ef7f92ce7f99a5d6be0702cb9e7a3b386c2b56c1
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-30 11:10:31 -0500

    Agregado interfaz activity

---
+++ src/app/housekeeper/interfaces/activity.interface.ts
@@ -0,0 +1,23 @@
+export interface BodyGetActivities {
+    status: number;
+    message: string;
+    body: Body;
+}
+
+export interface Body {
+    list: Activity[];
+    count: number;
+    page: number;
+    page_count: number;
+    page_size: number;
+}
+
+export interface Activity {
+    id: number;
+    dateAssigned: string;
+    title: string;
+    description: string;
+    status: string;
+    hourStart?: any;
+    hourEnd?: string;
+}
