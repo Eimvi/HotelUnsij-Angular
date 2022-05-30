883299fbc34c5e65d40cfa22be5b9a78b9d07aa7
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-30 11:11:31 -0500

    Agregado componentes menu y activities al NgModule

--- src/app/housekeeper/housekeeper.module.ts
+++ src/app/housekeeper/housekeeper.module.ts
@@ -6,13 +6,17 @@ import { HousekeeperRoutingModule } from './housekeeper-routing.module';
 import { HousekeeperComponent } from './housekeeper.component';
 import { HousekeeperModulesComponent } from './components/housekeeper-modules/housekeeper-modules.component';
 import { SharedModule } from '../shared/shared.module';
+import { HousekeeperMenuComponent } from './components/housekeeper-menu/housekeeper-menu.component';
+import { HousekeeperActivitiesComponent } from './components/housekeeper-activities/housekeeper-activities.component';
 
 
 
 @NgModule({
   declarations: [
     HousekeeperComponent,
-    HousekeeperModulesComponent
+    HousekeeperModulesComponent,
+    HousekeeperMenuComponent,
+    HousekeeperActivitiesComponent
   ],
   imports: [
     CommonModule,
