8f207f9c2223874ab613eda52a7c2cb9be0065d2
HRadry <hernandezramirezadriana1999@gmail.com>
2022-05-31 00:29:54 -0500

    Agregado componenetes de reporte previo, video y camara de ama de llaves

--- src/app/housekeeper/housekeeper.module.ts
+++ src/app/housekeeper/housekeeper.module.ts
@@ -6,18 +6,27 @@ import { HousekeeperRoutingModule } from './housekeeper-routing.module';
 import { HousekeeperComponent } from './housekeeper.component';
 import { HousekeeperModulesComponent } from './components/housekeeper-modules/housekeeper-modules.component';
 import { SharedModule } from '../shared/shared.module';
+import { HousekeeperPreviousReportComponent } from './components/housekeeper-previous-report/housekeeper-previous-report.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VideoFileComponent } from './video-file/video-file.component';
+import { CameraFilesComponent } from './camera-files/camera-files.component';
 
 
 
 @NgModule({
   declarations: [
     HousekeeperComponent,
-    HousekeeperModulesComponent
+    HousekeeperModulesComponent,
+    HousekeeperPreviousReportComponent,
+    VideoFileComponent,
+    CameraFilesComponent,
   ],
   imports: [
     CommonModule,
     HousekeeperRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
+
   ]
 })
 export class HousekeeperModule { }
