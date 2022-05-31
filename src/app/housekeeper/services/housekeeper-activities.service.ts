1a4614efaab605676e32edeeb29161f1548f6612
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-05-30 23:58:53 -0500

    Eliminado código ajeno a esta historia

--- src/app/housekeeper/services/housekeeper-activities.service.ts
+++ src/app/housekeeper/services/housekeeper-activities.service.ts
@@ -2,9 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { Body, BodyGetActivities } from '../interfaces/activity.interface';
-import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +12,7 @@ export class HousekeeperActivitiesService {
 
   private readonly URL: string = environment.URL;
 
-  constructor(private httpClient: HttpClient, private toastService: ToastrService) { }
+  constructor(private httpClient: HttpClient) { }
 
   getActivities(page: number = 1, keyword: string = ''): Observable<Body> {
     const query = { page, keyword };
@@ -21,24 +20,6 @@ export class HousekeeperActivitiesService {
       map((response) => {
         return response.body;
       })
-    );
-  }
-
-  previousReportCreate(previousReport: FormData, id:string){
-    const query = {
-      id
-    }
-    return this.httpClient.patch(`${this.URL}previous-report/update/`, previousReport,{params:query}).pipe(
-      tap(() => this.toastService.success('El reporte previo se guardo correctamente.'))
-    );
-  }
-
-  posteriorReportCreate(posteriorReport: FormData, id:string){
-    const query = {
-      id
-    }
-    return this.httpClient.patch(`${this.URL}posterior-report/update/`, posteriorReport,{params:query}).pipe(
-      tap(() => this.toastService.success('El reporte posterior se guardo correctamente.'))
     );
   }
 }
