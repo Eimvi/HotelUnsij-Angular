89c7a44f3ec66722790b08adb30e837c79939c83
HRadry <hernandezramirezadriana1999@gmail.com>
2022-06-09 23:46:27 -0500

    Agregado componente de entradas y salidas

---
+++ src/app/housekeeper/components/register/register.component.ts
@@ -0,0 +1,117 @@
+import { Component, OnInit, Input} from '@angular/core';
+import { ChamberMaid} from '../../interfaces/registerAmenidades.interface';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder} from '@angular/forms';
+import { Register } from '../../interfaces/registerAmenidades.interface';
+import { ProfileService } from 'src/app/auth/services/profile.service';
+import { HousekeeperAmenidadesService } from '../../services/housekeeper-amenidades.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  @Input() maidList!: Array<Body>;
+  public data: ChamberMaid[] = []
+  actualPage: number = this.route.snapshot.queryParams['page'];
+  searchTxt: string = '';
+  is_edit!: boolean;
+  profile = this.profileService.showData();
+
+  registerAmenidades!: FormGroup;
+
+
+  constructor(private router: Router, private route: ActivatedRoute, private toastService: ToastrService,
+    private housekeeperAmenidades: HousekeeperAmenidadesService, private fb: FormBuilder, private profileService: ProfileService) { this.is_edit = true; }
+
+  ngOnInit(): void {
+    this.registerAmenidades = this.fb.group({
+      id: [this.profile.id],
+      type: ['Salida'],
+      name: [this.profile.name],
+      toallas: [0],
+      sabanas: [0],
+      cobija: [0]
+    })
+  }
+
+  onOptionsSelected(value: string) {
+    if (value == 'Camarista') {
+      this.is_edit = false;
+      this.getMaid();
+    } else {
+      this.is_edit = true;
+      this.data = [];
+
+    }
+  }
+
+  option(id: string) {
+    const idMaid = this.data.findIndex(item => item.id == Number(id));
+    const maid = this.data[idMaid];
+    this.registerAmenidades.patchValue({ id: maid.id, name: maid.name })
+  }
+
+
+  onSubmit() {
+    const register : Register = {
+      id:this.registerAmenidades.get('id')?.value,
+      type:this.registerAmenidades.get('type')?.value,
+      name:this.registerAmenidades.get('name')?.value,
+
+      amenidades: [
+        {
+          name: 'toallas',
+          quantity: this.registerAmenidades.get('toallas')?.value
+        },
+        {
+          name: 'sabanas',
+          quantity: this.registerAmenidades.get('sabanas')?.value
+        },
+        {
+          name: 'cobija',
+          quantity: this.registerAmenidades.get('cobija')?.value
+        }
+      ]
+    }
+    this.housekeeperAmenidades.sendInfo(register).subscribe(
+      resp => {
+        this.router.navigate(['/housekeeper/amenityRecordsList']);
+      }
+    )
+
+  }
+
+  pageSelected(page: number): void {
+    this.router.navigate([],
+      {
+        relativeTo: this.route,
+        queryParams: { page: page },
+        queryParamsHandling: 'merge'
+      });
+    this.actualPage = page;
+    this.getMaid();
+  }
+
+
+  getMaid(): void {
+    this.housekeeperAmenidades.getMaid()
+      .subscribe(data => {
+        console.log(data)
+        this.data = data
+        console.log(data)
+      });
+
+  }
+
+  alertMessagePage(): void {
+    this.toastService.warning('La página solicitada no existe. 😥');
+    this.pageSelected(1);
+  }
+
+  submitForm() {
+  }
+
+}
