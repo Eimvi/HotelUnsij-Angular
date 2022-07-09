2fbc8bcf4017ce6f79633efee6dd1076e4efe85a
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-07-09 13:19:49 -0500

    Agregado ruta de componentes

--- src/app/attendant/attendant-routing.module.ts
+++ src/app/attendant/attendant-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuChildGuard } from '../core/guards/menu-child.guard';
+import { MenuGuard } from '../core/guards/menu.guard';
 import { AttendantComponent } from './attendant.component';
+import { CashDepositComponent } from './components/cash-deposit/cash-deposit.component';
+import { PettyCashComponent } from './components/petty-cash/petty-cash.component';
 
-const routes: Routes = [{ path: '', component: AttendantComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'petty-cash',
+    pathMatch: 'full'
+  },
+  {
+    path: '', component: AttendantComponent, canActivate: [MenuGuard],
+    children: [
+      {
+        path: 'petty-cash', component: PettyCashComponent, canActivateChild: [MenuChildGuard]
+      },
+      {
+        path: 'cash-deposit', component: CashDepositComponent, canActivateChild: [MenuChildGuard]
+      },
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
