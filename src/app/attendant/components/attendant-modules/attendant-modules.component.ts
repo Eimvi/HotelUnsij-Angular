9e39523c430e8b4f63e77d6f81de3a14daeec331
emmaGT <gtapiaemmanuel@gmail.com>
2022-07-09 20:18:55 -0500

    Agregado componente attendant-modules

---
+++ src/app/attendant/components/attendant-modules/attendant-modules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-attendant-modules',
+  templateUrl: './attendant-modules.component.html',
+  styleUrls: ['./attendant-modules.component.scss']
+})
+export class AttendantModulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
