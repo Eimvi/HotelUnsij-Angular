2c1d5f8e0a389227e02291952d02936a9161f936
Hipo-ui <hipolitojavierdominguez@gmail.com>
2022-07-10 21:42:34 -0500

    Agregado paginación y búsqueda

--- src/app/attendant/components/cash-deposit/cash-deposit.component.ts
+++ src/app/attendant/components/cash-deposit/cash-deposit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CashDepositService } from '../../services/cash-deposit.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Register } from '../../interfaces/cashDeposit.interface';
+import { CashDepositService } from '../../services/cash-deposit.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-cash-deposit',
@@ -8,18 +10,59 @@ import { Register } from '../../interfaces/cashDeposit.interface';
   styleUrls: ['./cash-deposit.component.scss']
 })
 export class CashDepositComponent implements OnInit {
-  registers!: Array<Register>;
 
-  constructor(private cashDepositService: CashDepositService) { }
+  registers!: Array<Register>;
+  pageSize: Array<number> = [];
+  actualPage: number = this.route.snapshot.queryParams['page'];
+  searchTxt: string = '';
+
+  constructor(private router: Router, private route: ActivatedRoute,
+    private toastService: ToastrService,
+    private cashDepositService: CashDepositService) { }
 
   ngOnInit(): void {
-    this.getRegisters(); 
+    this.getRegisters();
+  }
+
+
+  pageSelected(page: number): void {
+    this.router.navigate([],
+      {
+        relativeTo: this.route,
+        queryParams: { page: page },
+        queryParamsHandling: 'merge'
+      });
+    this.actualPage = page;
+    this.getRegisters();
   }
 
   getRegisters(): void {
-    this.cashDepositService.getRegisters()
-      .subscribe(data => {
-        this.registers = data;    
-      });
+    if (this.actualPage > 0) {
+      this.cashDepositService.getRegisters(this.actualPage, this.searchTxt)
+        .subscribe(data => {
+          this.registers = data.list;
+          this.pageSize.length = data.page_count;
+          // Verify query page exists
+          if (data.page_count) this.verifyPageExists(data.page_count);
+        });
+    } else {
+      this.alertMessagePage();
+    }
+  }
+
+  filterActivities(searchTxt: string): void {
+    this.searchTxt = searchTxt;
+    this.pageSelected(1);
+  }
+
+  verifyPageExists(page: number): void {
+    if (this.actualPage > page) {
+      this.alertMessagePage();
+    }
+  }
+
+  alertMessagePage(): void {
+    this.toastService.warning('La página solicitada no existe. 😥');
+    this.pageSelected(1);
   }
 }
