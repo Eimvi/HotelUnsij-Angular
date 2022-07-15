b3ff1e493643c68c76b55745b3d047f4140daea5
HRadry <hernandezramirezadriana1999@gmail.com>
2022-07-15 01:06:48 -0500

    Agregado componente de reporte de depósito, video y cámara

--- src/app/attendant/attendant.module.ts
+++ src/app/attendant/attendant.module.ts
@@ -3,15 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { AttendantRoutingModule } from './attendant-routing.module';
 import { AttendantComponent } from './attendant.component';
+import { DepositReportComponent } from './components/deposit-report/deposit-report.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../app-routing.module';
+import { VideoFileComponent } from './components/video-file/video-file.component';
+import { CameraFilesComponent } from './components/camera-files/camera-files.component';
 
 
 @NgModule({
   declarations: [
-    AttendantComponent
+    AttendantComponent,
+    DepositReportComponent,
+    VideoFileComponent,
+    CameraFilesComponent
   ],
   imports: [
     CommonModule,
-    AttendantRoutingModule
+    AttendantRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class AttendantModule { }
