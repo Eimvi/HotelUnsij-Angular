7fc64a9a5ee418124fedd2c641077000a101ae8e
HRadry <hernandezramirezadriana1999@gmail.com>
2022-07-15 01:06:12 -0500

    Agregado rutas de reporte de depósito, video y cámara

--- src/app/attendant/attendant-routing.module.ts
+++ src/app/attendant/attendant-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanActivateChild } from '@angular/router';
 import { AttendantComponent } from './attendant.component';
+import { DepositReportComponent } from './components/deposit-report/deposit-report.component';
+import { VideoFileComponent } from '../attendant/components/video-file/video-file.component';
+import { CameraFilesComponent } from '../attendant/components/camera-files/camera-files.component';
+import { MenuGuard } from '../core/guards/menu.guard';
+import { MenuChildGuard } from '../core/guards/menu-child.guard';
 
-const routes: Routes = [{ path: '', component: AttendantComponent }];
+const routes: Routes = [
+  {
+    path: '', component: AttendantComponent, canActivate: [MenuGuard],
+  children: [
+      {
+        path: 'depositReport', component: DepositReportComponent, canActivateChild:[MenuChildGuard]
+      },
+      {
+        path: 'deposit/video', component: VideoFileComponent, canActivateChild:[MenuChildGuard]
+      },
+      {
+        path: 'deposit/camera', component: CameraFilesComponent, canActivateChild:[MenuChildGuard]
+      }
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
